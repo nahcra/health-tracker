7a508242ee40f432d6fcde6ba995532e92d5e92a
nahcra <archankhadka91@gmail.com>
2025-06-01 21:15:13 -0400

    imtergrate firebase , add auth , update grocery

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXZgNctLWAu0haKvd90_hFAlo1WHM1n7Q",
-  authDomain: "myhealthtrackerapp.firebaseapp.com",
-  projectId: "myhealthtrackerapp",
-  storageBucket: "myhealthtrackerapp.firebasestorage.app",
-  messagingSenderId: "234509938064",
-  appId: "1:234509938064:web:ab0c8e5af909b83ee99715",
-  measurementId: "G-HZYD9L44TC"
+  apiKey: "AIzaSyYOUR_UNIQUE_API_KEY_HERE",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+  // measurementId: "G-YOUR_MEASUREMENT_ID" // Optional
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+// const analytics = firebase.analytics(); // Optional, if you enabled Analytics
