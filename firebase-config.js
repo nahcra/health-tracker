2033879dd2fa9460441aaafa3c3b55db00d353c6
nahcra <archankhadka91@gmail.com>
2025-06-01 22:06:59 -0400

    updated fb links

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- apiKey: "AIzaSyCXZgNctLWAu0haKvd90_hFAlo1WHM1n7Q",
+  apiKey: "AIzaSyCXZgNctLWAu0haKvd90_hFAlo1WHM1n7Q",
   authDomain: "myhealthtrackerapp.firebaseapp.com",
   projectId: "myhealthtrackerapp",
   storageBucket: "myhealthtrackerapp.firebasestorage.app",
   messagingSenderId: "234509938064",
-  appId: "1:234509938064:web:ab0c8e5af909b83ee99715",
-  measurementId: "G-HZYD9L44TC"
+  appId: "1:234509938064:web:ab0c8e5af909b83ee99715"
+  // measurementId: "G-HZYD9L44TC" // Optional
 };
 
 // Initialize Firebase
